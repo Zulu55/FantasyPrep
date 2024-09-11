39bf676ad75bae822c7638e9b1f14d08d9e86301
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-09-11 12:47:39 -0500

    Improve performance when GetAllGroups

--- Fantasy/Fantasy.Backend/Repositories/Implementations/GroupsRepository.cs
+++ Fantasy/Fantasy.Backend/Repositories/Implementations/GroupsRepository.cs
@@ -286,8 +286,7 @@ public class GroupsRepository : GenericRepository<Group>, IGroupsRepository
     public async Task<ActionResponse<IEnumerable<Group>>> GetAllAsync()
     {
         var groups = await _context.Groups
-            .Include(x => x.Members!)
-            .ThenInclude(x => x.User)
+            .Include(x => x.Admin)
             .Include(x => x.Tournament)
             .Where(x => x.IsActive)
             .OrderBy(x => x.Name)
